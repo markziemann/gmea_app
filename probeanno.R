09d203deec464f99c93862b6d809fe638eba677f
markziemann <mark.ziemann@gmail.com>
2024-06-27 00:13:51 +1000

    EPIC

--- probeanno.R
+++ probeanno.R
@@ -3,6 +3,7 @@ library("IlluminaHumanMethylationEPICanno.ilm10b4.hg19")
 library("HGNChelper")
 library("mitch")
 
+# 450K
 anno <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
 myann <- data.frame(anno[,c("UCSC_RefGene_Name","UCSC_RefGene_Group","Islands_Name","Relation_to_Island")])
 gp <- myann[,"UCSC_RefGene_Name",drop=FALSE]
@@ -26,4 +27,20 @@ fix2 <- fix[which(fix$x != fix$Suggested.Symbol),]
 length(unique(fix2$x))
 gt1$gene <- fix$Suggested.Symbol
 head(gt1)
-saveRDS(gt1,"450K,rds")
+saveRDS(gt1,"450K.rds")
+
+# EPIC
+anno <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
+myann <- data.frame(anno[,c("UCSC_RefGene_Name","UCSC_RefGene_Group","Islands_Name","Relation_to_Island")])
+gp <- myann[,"UCSC_RefGene_Name",drop=FALSE]
+gp2 <- strsplit(gp$UCSC_RefGene_Name,";")
+names(gp2) <- rownames(gp)
+gp2 <- lapply(gp2,unique)
+gt2 <- stack(gp2)
+colnames(gt2) <- c("gene","probe")
+gt2$probe <- as.character(gt2$probe)
+fix <- checkGeneSymbols(gt2$gene,map=new.hgnc.table)
+fix2 <- fix[which(fix$x != fix$Suggested.Symbol),]
+length(unique(fix2$x))
+gt2$gene <- fix$Suggested.Symbol
+saveRDS(gt2,"EPIC.rds")
