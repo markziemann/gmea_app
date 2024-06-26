735dc7fdd3b78d5c785babf9c78f1fb42a85f494
markziemann <mark.ziemann@gmail.com>
2024-06-26 23:14:41 +1000

    report works

--- gmea_app/app.R
+++ gmea_app/app.R
@@ -85,18 +85,21 @@ server <- function(input, output, session) {
     summary(unlist(lapply(data2(),length)))
   })
   output$prioritisation <- renderUI({
-    selectInput("prioritisation", "Prioritisation", choices = c("Effect","Significance"), selected = "Effect")
+    selectInput("prioritisation", "Prioritisation", 
+                choices = c("Effect","Significance"), selected = "Effect")
   })
   
   output$minsetsize <- renderUI({
-    selectInput("minsetsize", "Minimum gene set size", choices = c("5","10"), selected = "5")
+    selectInput("minsetsize", "Minimum gene set size", choices = c("5","10"), 
+                selected = "5")
   })
   
   output$chip_selector <- renderUI({
-    selectInput("chip", "Choose array chip", choices = c("450K","EPIC"), selected = "450K")
+    selectInput("chip", "Choose array chip", choices = c("450K","EPIC"), 
+                selected = "450K")
   })
   
-  enrichment_data <- reactive({
+  edata <- reactive({
     req(data(),data2(),input$chip)
     if (input$chip == "450K") {
       gt <- readRDS("450K.rds")
@@ -108,6 +111,11 @@ server <- function(input, output, session) {
     minsetsize <- as.numeric(input$minsetsize)
     mres <- mitch_calc(x=m,genesets=data2(),minsetsize=minsetsize, 
                        priority=tolower(input$prioritisation),cores=4)
+    return(mres)
+  })
+      
+  enrichment_data <- reactive({
+    mres <- edata() 
     return(mres$enrichment_result)
   })
   
@@ -117,18 +125,20 @@ server <- function(input, output, session) {
 
   plot_data1 <- reactive({
     edata <- head(subset(enrichment_data(),p.adjustANOVA<0.05),20)
-    if (nrow(edata)==0) {
-      plot.new()
-      text(0.5,0.5,labels="No significant sets found (FDR<0.05)")
+    if (nrow(edata)>1) {
+      edata <- edata[order(edata$s.dist),]
+      vec <- edata$s.dist
+      names(vec) <- edata$set
+      names(vec) <- substr(names(vec), start = 1, stop = 60)
+      names(vec) <- gsub("_"," ",names(vec))
+      par(mar=c(4,33,1,1))
+      barplot(abs(vec),col=sign(-vec)+3,horiz=TRUE,las=1,cex.names=1,
+              xlab="Enrichment Score")
+      mtext("Red=higher,Blue=lower methylation",side=3,adj=1)
+    } else {
+      plot(1,1,cex=0,xaxt = 'n', yaxt = 'n', bty = 'n', ylab = '', xlab = '')
+      text(1,1,labels="None found")
     }
-    edata <- edata[order(edata$s.dist),]
-    vec <- edata$s.dist
-    names(vec) <- edata$set
-    names(vec) <- substr(names(vec), start = 1, stop = 60)
-    names(vec) <- gsub("_"," ",names(vec))
-    par(mar=c(4,33,1,1))
-    barplot(abs(vec),col=sign(-vec)+3,horiz=TRUE,las=1,cex.names=1,xlab="Enrichment Score")
-    mtext("Red=higher,Blue=lower methylation",side=3,adj=1)
   })
   
   output$enrichmentplot1 <- renderPlot({
@@ -145,55 +155,14 @@ server <- function(input, output, session) {
     names(vec) <- substr(names(vec), start = 1, stop = 60)
     names(vec) <- gsub("_"," ",names(vec))
     par(mar=c(4,33,1,1))
-    barplot(abs(vec),col=sign(-vec)+3,horiz=TRUE,las=1,cex.names=1,xlab="Enrichment Score")
+    barplot(abs(vec),col=sign(-vec)+3,horiz=TRUE,las=1,cex.names=1,
+            xlab="Enrichment Score")
     mtext("Red=higher,Blue=lower methylation",side=3,adj=1)
   })
   
   output$enrichmentplot2 <- renderPlot({
     plot_data2()
   }, height = 500, width=600)
-  
-  regression_model <- reactive({
-    req(data(), input$x_axis, input$y_axis)
-    lm(formula = as.formula(paste(input$y_axis, "~", input$x_axis)), data = data())
-  })
-  
-  regression_stats <- reactive({
-    req(regression_model())
-    model <- regression_model()
-    summary_model <- summary(model)
-    
-    # Calculate correlation and its p-value
-    cor_test <- cor.test(data()[[input$x_axis]], data()[[input$y_axis]])
-    
-    list(
-      r_value = sqrt(summary_model$r.squared) * sign(summary_model$coefficients[2,1]),
-      r_squared = summary_model$r.squared,
-      adj_r_squared = summary_model$adj.r.squared,
-      f_statistic = summary_model$fstatistic[1],
-      p_value = summary_model$coefficients[2,4],
-      correlation = cor_test$estimate,
-      cor_p_value = cor_test$p.value
-    )
-  })
-  
-  output$regression_summary <- renderPrint({
-    req(regression_model(), regression_stats())
-    
-    cat("Regression Summary:\n\n")
-    cat("R value:", round(regression_stats()$r_value, 4), "\n")
-    cat("R-squared:", round(regression_stats()$r_squared, 4), "\n")
-    cat("Adjusted R-squared:", round(regression_stats()$adj_r_squared, 4), "\n")
-    cat("F-statistic:", round(regression_stats()$f_statistic, 4), "\n")
-    cat("P-value:", format.pval(regression_stats()$p_value, digits = 4), "\n\n")
-    
-    cat("Model Coefficients:\n")
-    print(summary(regression_model())$coefficients)
-    
-    cat("\nAdditional Statistics:\n")
-    cat("Correlation coefficient:", round(regression_stats()$correlation, 4), "\n")
-    cat("Correlation p-value:", format.pval(regression_stats()$cor_p_value, digits = 4), "\n")
-  })
   
   output$download_report <- downloadHandler(
     filename = function() {
@@ -203,11 +172,9 @@ server <- function(input, output, session) {
       tempReport <- file.path(tempdir(), "report.Rmd")
       file.copy("report.Rmd", tempReport, overwrite = TRUE)
       
-      params <- list(plot = plot_data(),
-                     stats = regression_stats(),
-                     x_var = input$x_axis,
-                     y_var = input$y_axis,
-                     coefficients = summary(regression_model())$coefficients)
+      params <- list("res"=edata(),
+                     "minsetsize"=input$minsetsize,
+                     "prioritisation"=input$prioritisation)
       
       rmarkdown::render(tempReport, output_file = file,
                         params = params,
